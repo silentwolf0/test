16144380ccc9cde2afecb4eb14f2af69e4bbb140
cetric okola <cetokola@outlook.com>
2018-11-13 06:07:45 +0300

    my first RESTful api

---
+++ RestfulApi.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	fmt.Println("Endpoint Hit: returnAllArticles")
+
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the homepage")
+	fmt.Println("Endpoint Hit:homepage")
+}
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/all", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":8087", nil))
+}
+func main() {
+	handleRequest()
+}
